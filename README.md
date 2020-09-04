cfb742a67efca0020addbd1951128ff04c91249f
Dora F <doradora.prog@gmail.com>
2020-09-05 00:43:12 +0900

    Beta version

---
+++ README.md
@@ -0,0 +1,15 @@
+# shinyintro
+
+某所でやるイントロ企画用のプログラム
+
+簡易的なシャニマスのガシャっぽい何か
+
+## slackbot_settings.py の中身
+```
+API_TOKEN = "Slackのトークン" # Token
+DEFAULT_REPLY = "夏川はいいぞ"
+PLUGINS = ['plugins']
+```
+
+## LICENSE
+[MIT](LICENSE)
--- plugins/default.py
+++ plugins/default.py
@@ -1,9 +1,35 @@
+import json
+import random
 from slackbot.bot import default_reply
 
 cnt = 0
 
+colors = {
+    "Vo": "#e05ab4",
+    "Da": "#59afe1",
+    "Vi": "#e0e05a"
+}
+
+with open('idols.json') as f:
+    idols = (json.load(f))["idols"]
+
 @default_reply()
 def default_func(msg):
-    global cnt
-    cnt += 1
-    msg.reply(f"HELLO! This is reply no.{cnt}.")
+    r = random.random()
+    rare = "R"
+    if r < 0.05:
+        rare = "SSR"
+    elif r < 0.21:
+        rare = "SR"
+    idol = random.choice(idols)
+
+    print(f"Put [{rare}]{idol['name']}")
+
+    attachments = [
+    {
+        'title': rare+idol["name"],
+        'text': f'Unit: {idol["unit"]}, Type: {idol["type"]}',
+        'color': colors[idol["type"]],
+        'image_url': idol["url"]
+    }]
+    msg.send_webapi('', json.dumps(attachments))
